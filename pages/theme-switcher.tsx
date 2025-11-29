e66e00084a7a18b800faf40ba3195929c5b025e7
mojtaba-bb <10mojijom01@gmail.com>
2025-11-29 10:04:42 -0700

    some works on theme switcher page

--- pages/theme-switcher.tsx
+++ pages/theme-switcher.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import "@/app/globals.css";
 import Link from "next/link";
+
+import { RxExit } from "react-icons/rx";
+import { CiShoppingCart } from "react-icons/ci";
 function themeSwitcher() {
+  
   return (
-    <div>
-        
+    <div className='w-screen min-h-screen flex flex-col bg-white'>
+      <div className='h-screen w-screen flex flex-col'>
+          <nav className='flex p-3 justify-between items-center'>
+
+          <ul className='flex gap-3  items-center text-sm  text-gray-500 '>
+            <li>
+              <h1 className='text-red-600 font-bold text-lg'>
+                Simple Shop
+              </h1>
+            </li>
+            <li>Home</li>
+          </ul>
+          <ul className='flex gap-3 text-lg'>
+            <li className='text-gray-500'><CiShoppingCart/></li>
+            <li className='text-red-600'><RxExit/></li>
+
+          </ul>
+        </nav>
+        <div className='w-full  flex-1 p-10'>
+            <div className='w-full h-full bg-linear-to-br from-slate-300 to-slate-200 rounded-2xl'>
+
+            </div>
+        </div>
+      </div>
+
     </div>
   )
 }
